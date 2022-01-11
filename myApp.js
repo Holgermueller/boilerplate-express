48ca3d64164ba93f9a44687ab5e36b2c0fbd7c64
Holger Mueller <9jholger@gmail.com>
2022-01-11 14:01:24 -0600

    solved six problems

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
+app.use(express.static(__dirname));
 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  }
+  res.json({ message: "Hello json" });
+});
 
+app.get("/", (req, res) =>
+  res.sendFile((absolutePath = __dirname + "/views/index.html"))
+);
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -27,22 +27,9 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use(express.static(__dirname));
-
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
-});
-
-app.get("/", (req, res) =>
-  res.sendFile((absolutePath = __dirname + "/views/index.html"))
-);
-
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   bGround.log("Node is listening on port " + port + "...");
-  console.log("Hello World");
 });
 
 /******************************************************
