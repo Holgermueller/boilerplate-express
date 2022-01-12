59627775694a73dff0dfef129b8834dd89fc25fc
Holger Mueller <9jholger@gmail.com>
2022-01-12 10:55:10 -0600

    more tests pased

--- myApp.js
+++ myApp.js
@@ -2,6 +2,32 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    console.log("Middle ware req.time = " + req.time);
+    next();
+  },
+  (req, res) => {
+    res.json({
+      time: req.time,
+    });
+  }
+);
+
+app.use((req, res, next) => {
+  let string = `${req.method} ${req.path} - ${req.ip} `;
+  console.log(string);
+  next();
+});
+
+console.log("Hello World");
+
+app.get("/", (req, res) =>
+  res.sendFile((absolutePath = __dirname + "/views/index.html"))
+);
+
 app.use(express.static(__dirname));
 
 app.get("/json", (req, res) => {
@@ -11,10 +37,11 @@ app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
 });
 
-app.get("/", (req, res) =>
-  res.sendFile((absolutePath = __dirname + "/views/index.html"))
-);
+app.get("/:word/echo", (req, res) => {
+  let word = req.params.word;
 
-console.log("Hello World");
+  let jsonObj = { echo: word, echo: word };
+  res.send(jsonObj);
+});
 
 module.exports = app;
